87b55117e88f12e6c3be1cca10a2d497d0efa88e
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-07-30 17:08:58 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const myVariable = 'Mathematics and Phisics'
+
+console.log(myVariable.length)
+console.log(1 + myVariable.charAt(15) + 2)
+
+console.log(myVariable.indexOf('ath'))
+
+console.log(myVariable.lastIndexOf('ic'))
+
+console.log(myVariable.slice(3, 8))
+
+console.log(myVariable.toUpperCase())
+
+console.log(myVariable.toLowerCase())
+
+console.log(myVariable)
+console.log(myVariable.includes('H'))
+
+console.log(myVariable.split(" "))
