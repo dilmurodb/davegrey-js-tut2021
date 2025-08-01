d4b8461c1872ef3d4644f665f3752eb4aa6593d9
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-08-01 10:24:19 -0400

    feat: rock paper scissors game

--- main.js
+++ main.js
@@ -1,15 +1,55 @@
-// User input
+// Your First Interactive Game
 
-// alert("Hello World!");
+let playGame = confirm("Shall we play rock, paper or scissors?");
 
-// let myBoolean = confirm("Ok === True\nCancel === False");
-// console.log(myBoolean);
-
-let userName = prompt("Please enter your name");
-if (userName) {
-    console.log(userName.length);
-    console.log(userName.trim().length)
-    console.log(userName.trim());
+if (playGame) {
+    // Play
+    let playerChoice = prompt("Please enter rock, paper or scissors")
+    if (playerChoice) {
+        // play
+        let playerOne = playerChoice.trim().toLowerCase();
+        if (playerOne === 'rock' || playerOne === 'paper' || playerOne === 'scissors') {
+            // play
+            let computerChoice = Math.floor(Math.random() * 3 + 1);
+            let computer = 
+            computerChoice === 1 
+                ? 
+            'rock' 
+                : 
+            computerChoice === 2 
+                ? 
+            'paper' 
+                : 
+            'scissors';
+            let result = 
+            playerOne === computer
+                ?
+            `PlayerOne: ${playerOne}\nComputer: ${computer}\nIt is a tie!`
+                :
+            playerOne === 'rock' && computer === 'paper'
+                ?
+            `PlayerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+                :
+            playerOne === 'paper' && computer === 'scissors'
+                ?
+            `PlayerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+                :
+            playerOne === 'scissors' && computer === 'rock'
+                ?
+            `PlayerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+                :
+            `PlayerOne: ${playerOne}\nComputer: ${computer}\nPlayerOne wins!`;
+            alert(result);
+            let playAgain = confirm("Play again?")
+            playAgain ? location.reload() : alert("Ok, thanks for playing!");
+        } else {
+            alert("You didn't enter rock, paper or scissors!")
+        }
+    } else if (playerChoice === '') {
+        alert("You have not entered rock, paper or scissors!")
+    } else {
+        alert("I guess you chabged your mind. Maybe next time")
+    }
 } else {
-    console.log("You didn't enter your name!");
+    alert("Ok, maybe next time.");
 };
