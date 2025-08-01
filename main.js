d24da3cf6516e5bc7f889045cf0b019b13e9ef3e
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-08-01 12:12:11 -0400

    feta: loops

--- main.js
+++ main.js
@@ -1,55 +1,46 @@
-// Your First Interactive Game
+// Loops
 
-let playGame = confirm("Shall we play rock, paper or scissors?");
+let myNumber = 0;
 
-if (playGame) {
-    // Play
-    let playerChoice = prompt("Please enter rock, paper or scissors")
-    if (playerChoice) {
-        // play
-        let playerOne = playerChoice.trim().toLowerCase();
-        if (playerOne === 'rock' || playerOne === 'paper' || playerOne === 'scissors') {
-            // play
-            let computerChoice = Math.floor(Math.random() * 3 + 1);
-            let computer = 
-            computerChoice === 1 
-                ? 
-            'rock' 
-                : 
-            computerChoice === 2 
-                ? 
-            'paper' 
-                : 
-            'scissors';
-            let result = 
-            playerOne === computer
-                ?
-            `PlayerOne: ${playerOne}\nComputer: ${computer}\nIt is a tie!`
-                :
-            playerOne === 'rock' && computer === 'paper'
-                ?
-            `PlayerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
-                :
-            playerOne === 'paper' && computer === 'scissors'
-                ?
-            `PlayerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
-                :
-            playerOne === 'scissors' && computer === 'rock'
-                ?
-            `PlayerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
-                :
-            `PlayerOne: ${playerOne}\nComputer: ${computer}\nPlayerOne wins!`;
-            alert(result);
-            let playAgain = confirm("Play again?")
-            playAgain ? location.reload() : alert("Ok, thanks for playing!");
-        } else {
-            alert("You didn't enter rock, paper or scissors!")
-        }
-    } else if (playerChoice === '') {
-        alert("You have not entered rock, paper or scissors!")
-    } else {
-        alert("I guess you chabged your mind. Maybe next time")
-    }
-} else {
-    alert("Ok, maybe next time.");
+while (myNumber < 50) {
+    console.log(myNumber);
+    // myNumber = myNumber + 1;
+    // myNumber++;
+    myNumber += 5
 };
+
+let anotherNumber = 2;
+do {
+    anotherNumber += 2;
+    console.log(anotherNumber)
+} while (anotherNumber < 20);
+
+let personName = "Dilmurod Bukharov";
+for (let i = 0; i < personName.length; i++) {
+    console.log(`${i}: ${personName.charAt(i)}`)
+}
+
+let personAddress = 'Slippery rock';
+let counter = 0;
+
+while(true) {
+    let addressLetter = personAddress[counter];
+    console.log(addressLetter);
+    if (addressLetter === 'y') break;
+    counter++;
+}
+
+let word = "Great"
+let index = 0;
+
+while(index <= 3) {
+    let wordLetter = word[index];
+    console.log(wordLetter);
+    if (index === 1) {
+        index += 2;
+        continue;
+    }
+    if (wordLetter === 'e') break;
+    index ++
+}
+console.log(index);
