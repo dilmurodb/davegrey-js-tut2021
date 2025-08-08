f0d769ee16e5ca96e3f7f76dc2d289370f389215
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-08-07 21:30:01 -0400

    feat: JSON

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-// JavaScript Classes
+// JSON JavaScript Object Notation
+// JSON is used to send and receive data.
+// JSON is a text format that is completely language independent.
+// Meaning JSON is used to send and receive data in many languages
+// ... not just in JavaScript
 
-class Pizza {
-    constructor(pizzaType, pizzaSize) {
-        this.type = pizzaType,
-        this.size = pizzaSize,
-        this.crust = "original",
-        this.toppings = [];
+const myObj = {
+    name: "Dilmurod",
+    hobbies: ["eat", "sleep", "code"],
+    hello: function () {
+        console.log("Hello!")
     }
-    get pizzaCrust() {
-        return this.crust;
-    }
-    set pizzaCrust(pizzaCrust) {
-        this.crust = pizzaCrust;
-    }
-    bake() {
-        console.log(`Baking a ${this.size} ${this.crust} crust ${this.type} pizza.`)
-    }
-};
+}
+console.log(myObj);
+console.log(myObj.name);
+console.log(myObj.hobbies);
+myObj.hello();
+console.log(typeof myObj);
+const sendJSON = JSON.stringify(myObj);
+console.log(sendJSON);
+console.log(typeof sendJSON);
+console.log(sendJSON.name);
 
-const myPizza = new Pizza("pepperoni", "small");
-// myPizza.type = "supreme" // Not the right way - not recommneded
-myPizza.pizzaCrust = "thin"
-myPizza.bake();
-console.log(myPizza.size)
+const receiveJSON = JSON.parse(sendJSON);
+console.log(receiveJSON);
+console.log(typeof receiveJSON);
